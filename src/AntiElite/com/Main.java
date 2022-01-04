a6c587f4472fc96dfd658413c1eee8214bb184af
Nametsego <Nametsegothakadu@gmail.com>
2022-01-04 05:01:11 +0200

    Initial Commit

---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,13 @@
+package AntiElite.com;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        while(true){
+        JOptionPane.showMessageDialog(null,"Warning! this file contains explosive malware","Warning",JOptionPane.WARNING_MESSAGE);
+     }
+    }
+}
